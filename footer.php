05390e33c843c22a24344757bcb730ca77b5645d
xthy <xthy@me.com>
2013-12-19 22:06:25 +0900

    file upload

---
+++ footer.php
@@ -0,0 +1,30 @@
+<div id="bottom-container">
+	<div id="footer-wrapper">
+		<div id="footer">
+			<p>
+			&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
+			<?php wp_register(' | ',''); ?> | <?php wp_loginout(); ?><br/>
+			Proudly Powered by <a href="http://wordpress.org/">WordPress</a>
+			with "tanzaku" WordPress theme by <a href="http://www.tripleships.com/">TRIPLESHIPS.Inc</a></br>
+ (THTYLE CUSTOMIZED Ver., Optimized for a screen-resolution greater than or equal to 1280x1024 px)
+			</p>
+		</div>
+	</div>
+</div><!-- /bottom-container -->
+
+<?php wp_footer(); ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28323170-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+</html>
